181d129c4a62bb116607182228e44e0d855e6dbd
Sergio Moreno <sergio.moreno.calzada@everis.com>
2018-01-02 01:52:02 +0100

    Make Epoch time static, so it is not recreated each time

--- src/Api/Utils/DateTime.cs
+++ src/Api/Utils/DateTime.cs
@@ -4,9 +4,10 @@ namespace Boukenken.Gdax
 {
     public static class DateTimeUtilities
     {
+        public static DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
         public static double ToUnixTimestamp(this DateTime dateTime)
         {
-            return (dateTime - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds;
+            return (dateTime - Epoch).TotalSeconds;
         }
     }
 }
